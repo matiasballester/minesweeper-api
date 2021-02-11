82a410174000c1796464cb582fa410613b493415
Matias Ballester <ballestermh@gmail.com>
2021-02-11 12:11:12 -0300

    Add api documentation

--- src/main/java/com/mballester/minesweeper/controller/MinesWeeperController.java
+++ src/main/java/com/mballester/minesweeper/controller/MinesWeeperController.java
@@ -9,7 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/minesweeper")
@@ -94,4 +100,5 @@ public class MinesWeeperController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
+
 }
